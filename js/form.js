21018eb3db45b65756a51892094689ce03cfcf6f
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-09-16 20:05:33 -0300

    Refatoração de código: Implementando novas funções

--- js/form.js
+++ js/form.js
@@ -2,41 +2,17 @@ var form = document.querySelector("#formList");
 var botao = document.querySelector("#botao");
 var tabela = document.querySelector("#conteudo");
 
+
     botao.addEventListener('click', function(event){
         event.preventDefault();
-        var paciente = extrairForm(form);
+        
         
        if(!paciente.nome || !paciente.peso || !paciente.altura || !paciente.gorCorp){
            alert('Digite Todos os Campos!');
        }else{
-           
-            var pacienteTr = document.createElement("tr");
-            pacienteTr.classList.add("paciente");
-
-            var nomeTd = document.createElement("td");
-            nomeTd.classList.add("info-nome");
-
-            var pesoTd = document.createElement("td");
-            pesoTd.classList.add("info-peso");
-
-            var alturaTd = document.createElement("td");
-            alturaTd.classList.add("info-altura");
-
-            var gorCorpTd = document.createElement("td");
-            gorCorpTd.classList.add("info-gorCorp");
-
-            var imcTd = document.createElement("td");
-            imcTd.classList.add("info-imc");
-
-           
-            pacienteTr.appendChild(nomeTd);
-            pacienteTr.appendChild(pesoTd);
-            pacienteTr.appendChild(alturaTd);
-            pacienteTr.appendChild(gorCorpTd);
-            pacienteTr.appendChild(imcTd);
-
-            tabela.appendChild(pacienteTr);
-
+        var paciente = extrairForm(form);
+        var pacienteTr = montaTr(paciente);
+        tabela.appendChild(pacienteTr);
        }
 
     });
@@ -58,11 +34,13 @@ var tabela = document.querySelector("#conteudo");
         var pacienteTr = document.createElement("tr");
         pacienteTr.classList.add("paciente");
 
-        var nomeTd = montaTd(paciente.nome, "info-nome");
-        var pesoId = montaTd(paciente.peso, "info-peso");
-        var alturaTd = montaTd(paciente.altura, "info-altura");
-        var gorCorpTd = montaTd(paciente.gorCorp, "info-gorCorp");
-        var imcTd = montaTd(paciente.imc, "info-imc");
+        pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+        pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+        pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+        pacienteTr.appendChild(montaTd(paciente.gorCorp, "info-gorCorp"));
+        pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
+        
+        return pacienteTr;
         
     }
 
